b53ed9d5778d139539a8447f35d5239c5641a8c2
Matt Dewey <mattfromseattle@me.com>
2017-01-31 18:19:11 -0800

    Updating map and removing links and map from tabs.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
-/* Begin by adding your on ready handler here, and then create the
-   rest of your functions inside the on ready handler.
-
-   (Note that you do not need to manually call Bootstrap functions in
-   your Javascript because Bootstrap will automatically recognize your
-   HTML structures and invoke the proper JS code accordingly. Be sure
-   to reference the Bootstrap documentation.)
-*/
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    console.log( "ready!" );
+    
 
 // TODO: Inside of your on ready handler, invoke the Leaflet.js library
 // to draw a map in your `#map-container` div.
+              var mymap = L.map('mapid').setView([46.8503956, -121.7750116], 13);
+
+              L.tileLayer(
+                'https://api.mapbox.com/styles/v1/mattfromseattle/ciyjjb3kf000a2rphe8bomoll/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWF0dGZyb21zZWF0dGxlIiwiYSI6ImNpeWpqOTVrcDA1Njkyd29ldjViaW50eDcifQ.3lxfg0LRGoHG7PVVLC6J9Q', {
+                  maxZoom: 18,
+                  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+                    '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                    'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+                  id: 'mapbox.streets'
+                }).addTo(mymap);
 
 // TODO: Add 2 layers to your map you have visuals. Use the Open Street Maps
 // tiles served through the MapQuest CDN. Consult this example to set up
@@ -19,3 +25,4 @@
 // interesting locations. (You'll need to figure out the latitude/longitude for
 // these locations using a mapping tool such as Google Maps.)
 
+});
